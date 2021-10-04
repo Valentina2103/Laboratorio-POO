7efba5ccacfdd49fcf84fc17a7a7ec899bc5b335
Valentina Penuela Navas <valentina.penuela.2021@upb.edu.co>
2021-10-04 16:40:54 -0500

    Agregar archivos de proyecto.

---
+++ POO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+
+
+
